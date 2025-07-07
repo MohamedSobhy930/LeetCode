73e9b416c3e531e109dc1ffbd570f663e6f1cbe5
MohamedSobhy930 <mohamedsobhy332005@gmail.com>
2025-07-07 19:13:07 +0300

    Time: 875 ms (11.5%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select name from SalesPerson
+where sales_id not in(
+    select distinct o.sales_id from 
+    Orders o join Company c 
+    on o.com_id = c.com_id
+    where c.name = 'RED'
+)
