224917ec42cabf49404f71286f0a46a887f11362
MohamedSobhy930 <mohamedsobhy332005@gmail.com>
2025-07-17 14:36:44 +0300

    Time: 21 ms (15.77%), Space: 49.5 MB (89.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,19 +1,10 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        if(nums.Length <= 2) return nums[0];
-       Dictionary<int,int> freq = new Dictionary<int,int>();
-       for(int i = 0 ; i < nums.Length ;i++)
-       {
-        if(!freq.ContainsKey(nums[i]))
-            freq[nums[i]] = 0;
-        freq[nums[i]]++;
-       } 
-       int k = nums.Length / 2;
-       foreach(var pair in freq)
-       {
-        if(pair.Value > k)
-            return pair.Key;
-       }
-       return -1;
+       Array.Sort(nums);
+       int n = nums.Length;
+    if(nums[0] == nums[n/2])
+        return nums[0];
+    else 
+        return nums[n/2];
     }
 }
