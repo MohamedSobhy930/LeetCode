204385a2d6bd88e445bc7bce4b92920816be933c
MohamedSobhy930 <mohamedsobhy332005@gmail.com>
2025-07-24 03:19:24 +0300

    Time: 2 ms (64.82%), Space: 60.8 MB (89.54%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int farthest = 0;
+        for(int i = 0 ; i < nums.Length ; i++)
+        {
+            if(i > farthest) return false;
+            farthest = Math.Max(farthest, i + nums[i]);
+        }
+        return true;
+    }
+}
