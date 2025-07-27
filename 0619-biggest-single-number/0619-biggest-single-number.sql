3316ae0e563820761616539f7d4187d83335d591
MohamedSobhy930 <mohamedsobhy332005@gmail.com>
2025-07-27 21:38:38 +0300

    Time: 318 ms (76.59%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,6 +1,8 @@
 /* Write your T-SQL query statement below */
-select isnull((select top 1 num from MyNumbers
+select isnull((
+select top 1 num from MyNumbers
 Group by num 
 having Count(num) = 1
-order by num desc),null)
+order by num desc)
+,null)
 as num
