4c305088a51bf15e76fca9cfb72f377ec0c676b3
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-09-27 06:26:20 +0300

    Time: 2 ms (97.37%), Space: 42.3 MB (52.63%) - LeetHub

---
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public double LargestTriangleArea(int[][] points) {
+        double maxArea = 0 ; 
+        for(int i = 0 ; i < points.Length ; i++)
+        {
+            for(int j = i+1 ; j < points.Length ; j++)
+            {
+                for(int k = j+1 ; k < points.Length ; k++)
+                {
+                    double area = 0.5 * Math.Abs(points[i][0]*(points[j][1]-points[k][1]) 
+                    + points[j][0]*(points[k][1]-points[i][1]) 
+                    + points[k][0]*(points[i][1]-points[j][1]) ) ; 
+                    maxArea = Math.Max(maxArea , area);
+                }
+            }
+        }
+        return maxArea;
+    }
+}
