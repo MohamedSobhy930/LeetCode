5b33eb8f2395edfcc515b1f91ef0571addc91316
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-10-11 11:00:57 +0300

    Time: 1 ms (100%), Space: 72 MB (34.18%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
@@ -1,16 +1,13 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
-        Dictionary<int,int> unique = new Dictionary<int,int>();
-        for(int i = 0 ; i < nums.Length ; i++)
+        int n = nums.Length;
+        int[] unique = new int[n+1];
+        for(int i = 0 ; i < n ; i++)
         {
-            if(!unique.ContainsKey(nums[i]))
-            {
+            if(unique[nums[i]] == 0)
                 unique[nums[i]] = 1;
-            }
             else 
-            {
                 return nums[i];
-            }
         }
         return -1;
     }
