470bb2e3e497e1bd7a89ea08d4b9a2ace392bb44
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-10-12 10:37:33 +0300

    Time: 4 ms (62.16%), Space: 47.9 MB (12.37%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cs
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cs
@@ -1,15 +1,12 @@
 public class Solution {
     public int[] Intersection(int[] nums1, int[] nums2) {
-        Array.Sort(nums1);
-        Array.Sort(nums2);
-        List<int> result = new List<int>();
+        HashSet<int> result = new HashSet<int>();
         HashSet<int> resultSet = new HashSet<int>(nums1);   
         for(int i = 0 ; i < nums2.Length ; i++)
         {
             if(resultSet.Contains(nums2[i]))
             {
                 result.Add(nums2[i]);
-                resultSet.Remove(nums2[i]);
             }
         }
         return result.ToArray();
