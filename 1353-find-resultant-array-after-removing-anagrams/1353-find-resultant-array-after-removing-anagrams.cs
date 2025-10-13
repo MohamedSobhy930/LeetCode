29add0b5ab74abf0b481bd623d4cdb45496e41a6
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-10-13 11:08:08 +0300

    Time: 9 ms (28.57%), Space: 51.2 MB (39.29%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public IList<string> RemoveAnagrams(string[] words) {
+        var result = new List<string>();
+        result.Add(words[0]);
+        int j = 0; 
+        for(int i = 1 ; i < words.Length ; i++)
+        {
+            if(!AreAnagramsBySorting(words[j],words[i]))
+            {
+                result.Add(words[i]);
+                j = i ; 
+            }
+
+        }
+        return result;
+    }
+    public bool AreAnagramsBySorting(string s1, string s2) {
+    if (s1.Length != s2.Length) return false;
+
+    char[] a1 = s1.ToCharArray();
+    char[] a2 = s2.ToCharArray();
+
+    Array.Sort(a1);
+    Array.Sort(a2);
+
+    return new string(a1) == new string(a2);
+}
+}
