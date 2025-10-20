4f3a5e7887469b53912c4dbc788535836b2d9afc
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-10-20 11:12:03 +0300

    Time: 0 ms (100%), Space: 43 MB (34.53%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int FinalValueAfterOperations(string[] operations) {
+        int output = 0 ;
+        for(int i = 0 ; i < operations.Length ; i++)
+        {
+            if(operations[i][1] == '+')
+                output++;
+            else
+                output--;
+        }
+        return output;
+    }
+}
