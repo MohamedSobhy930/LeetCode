a2313ab664817eb77e7e1216e23daf077123f580
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-10-25 16:14:08 +0300

    Time: 0 ms (100%), Space: 28.9 MB (75.86%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int TotalMoney(int n)
+{
+    if (n <= 7) return n * (n + 1) / 2;
+    int sum = 28;
+    int weeks = n / 7;
+    for (int i = 2; i <= weeks; i++)
+    {
+        int weakSum = 7 * (2 * i + 6 ) / 2;
+        sum += weakSum;
+    }
+    n %= 7;
+    weeks++;
+    for(int i = 0; i < n; i++)
+    {
+        sum += weeks;
+        weeks++;
+    }
+    return sum;
+}
+}
