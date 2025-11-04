75fc04ddda235aacd5f62f103bafb25ec7f522e3
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-11-04 15:05:16 +0200

    Time: 67 ms (9.09%), Space: 56.4 MB (40.91%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cs
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cs
@@ -2,28 +2,39 @@ public class Solution {
     public int[] FindXSum(int[] nums, int k, int x) {
         int[] result = new int[nums.Length - k + 1];
         int n = nums.Length;
-        for(int i = 0 ; i < n - k + 1 ; i++)
+        var freq = new Dictionary<int,int>();
+        for(int i = 0 ; i < k ; i++)
         {
-            var freq = new Dictionary<int,int>();
-            int j = i ; 
-            while(j < k + i)
-            {
-                if(!freq.ContainsKey(nums[j]))
-                    freq[nums[j]] = 0;
-                freq[nums[j]]++;
-                j++;
-            }
-            int xSum = 0;
-            var topX = freq.OrderByDescending(pair => pair.Value)
-                        .ThenByDescending(pair => pair.Key)
-                        .Take(x)
-                        .ToList();
-            foreach(var item in topX)
-            {
-                xSum += item.Key * item.Value;
-            }
-            result[i] = xSum;
+            if(!freq.ContainsKey(nums[i]))
+                    freq[nums[i]] = 0;
+                freq[nums[i]]++;
+        }
+        result[0] = CalculateXsum(freq , x);
+        for(int i = 1 ; i < n - k + 1 ; i++)
+        {
+            freq[nums[i-1]]--;
+            if(freq[nums[i-1]] == 0)
+                freq.Remove(nums[i-1]);
+
+            if(!freq.ContainsKey(nums[i + k-1]))
+                freq[nums[i + k-1]] = 0;
+            freq[nums[i + k-1]]++;
+
+            result[i] = CalculateXsum(freq , x);
         }
         return result;
+    }
+    public int CalculateXsum(Dictionary<int,int> currFreq , int x)
+    {
+        int xSum = 0;
+        var topX = currFreq.OrderByDescending(pair => pair.Value)
+                    .ThenByDescending(pair => pair.Key)
+                    .Take(x)
+                    .ToList();
+        foreach(var item in topX)
+        {
+            xSum += item.Key * item.Value;
+        }
+        return xSum;
     }
 }
