5666bb039759c6347c5f396b3fd207aa173b889f
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2025-12-04 09:07:03 +0200

    Time: 7 ms (80%), Space: 47.3 MB (6.67%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int CountCollisions(string directions) {
+        int n = directions.Length ;
+        int left = 0 ;
+        int right = n -1; 
+        while(left < n && directions[left] == 'L')
+            left++;
+        while(right >= 0 && directions[right] == 'R')
+            right--;
+        int collisions = 0;
+        for(int i = left ; i <= right; i++)
+        {
+            if(directions[i] != 'S')
+                collisions++;
+        }
+        return collisions;
+    }
+}
