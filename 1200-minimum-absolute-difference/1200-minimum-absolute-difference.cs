66d40e77e619b8ac086475bc1b86e5dfe9c439e6
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-01-27 01:06:33 +0200

    Time: 51 ms (43.48%), Space: 64.3 MB (44.93%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cs
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cs
@@ -2,12 +2,13 @@ public class Solution {
     public IList<IList<int>> MinimumAbsDifference(int[] arr) {
         Array.Sort(arr);
         int minAbsDiff = int.MaxValue;
-        for(int i = 1 ; i < arr.Length ; i++)
+        int n = arr.Length ;
+        for(int i = 1 ; i < n ; i++)
         {
             minAbsDiff = Math.Min(minAbsDiff , arr[i] - arr[i-1]);
         }
         var result = new List<IList<int>>();
-        for(int i = 1 ; i < arr.Length ; i++)
+        for(int i = 1 ; i < n ; i++)
         {
             if(arr[i] - arr[i-1] == minAbsDiff)
                 result.Add(new List<int> {arr[i-1] , arr[i]});
