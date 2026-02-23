bcd75e17562095b4bddc7b50d0fa0af14770a72c
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-02-23 07:20:50 +0200

    Time: 111 ms (68.75%), Space: 102.4 MB (6.25%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cs
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public bool HasAllCodes(string s, int k) {
         HashSet<string> codes = new HashSet<string>();
-        int totalcodes = Convert.ToInt32(Math.Pow(2,k));
+        int totalcodes = 1 << k;
         for(int i = 0 ; i <= s.Length - k; i++)
         {
             string sub = s.Substring(i , k);
-            if(!codes.Contains(sub))
-                codes.Add(sub);
+            codes.Add(sub);
             if(codes.Count == totalcodes)
                 return true;
         }
