519d15d54522b35094c6234d29f290cbd6fa1e1c
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-03-03 07:03:41 +0200

    Time: 0 ms (100%), Space: 42 MB (58.62%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cs
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cs
@@ -1,26 +1,17 @@
 public class Solution {
-    public StringBuilder result = new StringBuilder("0");
     public char FindKthBit(int n, int k) {
-        if(n < 1 || result.Length >= k)
-            return result[k-1];
-        string s = result.ToString();
-        result.Append("1" + Reverse(Invert(s)));
-        return FindKthBit(n-1 , k);
-        
-    }
-    private string Reverse(string s)
-    {
-        char[] charArray = s.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
-    }
-    private string Invert(string s)
-    {
-        char[] charArray = s.ToCharArray();
-        for(int i = 0 ; i < s.Length ; i++)
+        if(n == 1) return '0';
+        int length = (1 << n) - 1;
+        int mid = length / 2 + 1;
+
+        if(k == mid)
+            return '1';
+        if( k > mid)
         {
-            charArray[i] = s[i] == '0' ? '1' : '0';
+            int mirroredK = length - k + 1;
+            return FindKthBit(n - 1 , mirroredK) == '0' ? '1' : '0';
         }
-        return new string(charArray);
+
+        return FindKthBit(n - 1 , k);   
     }
 }
