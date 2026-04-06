060630d72603f30b39fefa4bd28e6be406dfcbca
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-04-07 01:07:29 +0200

    Time: 3 ms (8.57%), Space: 41.6 MB (74.29%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cs
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cs
@@ -1,34 +1,26 @@
 public class Solution {
     public int FillCups(int[] amount) {
         var maxHeap = new PriorityQueue<int,int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
-        int totalAmount = 0;
         foreach(int cups in amount)
         {
-            totalAmount += cups;
             if(cups > 0)
                 maxHeap.Enqueue(cups , cups);
         }
         int seconds = 0;
-        while(totalAmount > 0)
+        while(maxHeap.Count > 1)
         {
-            if(maxHeap.Count >= 2)
-            {
-                maxHeap.TryDequeue(out int a , out int cupA);
-                maxHeap.TryDequeue(out int b , out int cupB);
-                totalAmount -= 2;
-                if(a > 1)
-                    maxHeap.Enqueue(a-1,a-1);
-                if(b > 1)
-                    maxHeap.Enqueue(b-1,b-1);
-            }
-            else 
-            {
-                maxHeap.TryDequeue(out int a , out int cupA);
-                totalAmount -= 1;
-                if(a > 1)
-                    maxHeap.Enqueue(a-1,a-1);
-            }
+            int first = maxHeap.Dequeue();
+            int second = maxHeap.Dequeue();
+
             seconds++;
+            if(first > 1)
+                maxHeap.Enqueue(first-1,first-1);
+            if(second > 1)
+                maxHeap.Enqueue(second-1,second-1);
+        }
+        if(maxHeap.Count == 1)
+        {
+            seconds += maxHeap.Dequeue();
         }
         return seconds;
     }
