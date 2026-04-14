912eaf46909f2cab8d5448ace2c6dba6291eeaea
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-04-14 14:11:18 +0200

    Time: 15 ms (34.17%), Space: 66.3 MB (22.59%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cs
+++ 0041-first-missing-positive/0041-first-missing-positive.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
         HashSet<int> map = new HashSet<int>();
-        long maxNum = int.MinValue;
+        int maxNum = int.MinValue;
+        int n = nums.Length;
         for(int i = 0 ; i < nums.Length ; i++)
         {
-            map.Add(nums[i]);
-            if(nums[i] > maxNum)
-                maxNum = nums[i];
+            if(nums[i] > 0 && nums[i] <= n)
+            {
+                map.Add(nums[i]);
+                if(nums[i] > maxNum)
+                    maxNum = nums[i];
+            }
         }
         if(maxNum < 1) return 1;
-        for(int i = 1 ; i <= maxNum + 1 ; i++)
+        for(int i = 1 ; i <= n ; i++)
         {
             if(!map.Contains(i))
                 return i;
         }
-        return 1;
+        return n+1;
     }
 }
