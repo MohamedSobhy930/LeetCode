d403c7ed8524cf35cbef713eea35d3f8ec442bdd
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-04-15 17:03:34 +0200

    Time: 0 ms (100%), Space: 41.5 MB (34.02%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int LargestAltitude(int[] gain) {
+        int highestAltitude = 0;
+        int altitude = 0;
+        for(int i = 0 ; i < gain.Length ; i++)
+        {
+            altitude += gain[i];
+            highestAltitude = Math.Max(highestAltitude , altitude);
+        }
+        return highestAltitude;
+    }
+}
