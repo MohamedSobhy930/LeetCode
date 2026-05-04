493364a652a59b4232d301da4933420ce8f36e6a
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-05-04 17:34:16 +0300

    Time: 0 ms (100%), Space: 46.8 MB (61.42%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int left = 0, right = matrix.Length - 1;
+        while(left < right)
+        {
+            int top = left , bottom = right;
+            for(int i = 0 ; i < right-left ; i++)
+            {
+                int temp = matrix[top][left + i];
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = temp;
+            }
+            left++;
+            right--;
+        }
+    }
+}
