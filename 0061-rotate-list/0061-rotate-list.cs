cd8eac5ab7c2fa4c5535fa464f82da803dbf069d
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-05-05 18:22:47 +0300

    Time: 0 ms (100%), Space: 42.7 MB (75.5%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cs
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
+        int count = 0;
+        ListNode cur = head;
+        while(cur != null && cur.next != null)
+        {
+            cur = cur.next;
+            count++;
+        }
+        k = k % (count + 1);
+        if(k == 0) return head;
+        cur = head;
+        int remaining = count - k;
+        while(remaining > 0)
+        {
+            cur = cur.next;
+            remaining--;
+        }
+        ListNode newHead = cur.next;
+        cur.next = null;
+        cur = newHead;
+        while(cur.next != null)
+        {
+            cur = cur.next;
+        }
+        cur.next = head;
+        head = newHead;
+        return head;
+    }
+}
