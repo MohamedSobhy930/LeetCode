c897b5f30388f2ed7f5bade02efc2c32f87d3fef
MohamedSobhy930 <mohammed.sobhy.fci@gmail.com>
2026-06-09 04:25:11 +0300

    Time: 4 ms (91.8%), Space: 87.7 MB (80.33%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cs
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cs
@@ -2,18 +2,19 @@ public class Solution {
     public int[] PivotArray(int[] nums, int pivot) {
        int n = nums.Length;
        int index = 0;
+       int pivotCount = 0;
        int[] result = new int[n];
-       List<int> pivotFreq = new List<int>();
         for(int i = 0 ; i < n ; i++)
         {
             if(nums[i] < pivot) 
                 result[index++] = nums[i];
             else if(nums[i] == pivot)
-                pivotFreq.Add(nums[i]);
+                pivotCount++;
         }
-        foreach(int num in pivotFreq)
+        while(pivotCount > 0)
         {
-            result[index++] = num;
+            result[index++] = pivot;
+            pivotCount--;
         }
         for(int i = 0 ; i < n ; i++)
         {
